78aa6f97942f60cd322a28976dee7727fcddb35a
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-01-22 21:08:16 +0100

    update init_db.sh

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -7,10 +7,10 @@ service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS ${WP_NAME};"
 mysql -e "CREATE USER IF NOT EXISTS '${WP_ADMIN_LOGIN}'@'%' IDENTIFIED BY '${WP_ADMIN_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${WP_NAME}.* TO '${WP_ADMIN_LOGIN}'@'%';"
-mysql -uroot -proot12345 -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';"
+mysql -u${DB_USER} -p${DB_PASSWORD} -e "ALTER USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 
 # Shut down MySQL service cleanly
-mysql -uroot -proot12345 -e "SHUTDOWN;"
+mysql -u${DB_USER} -p${DB_PASSWORD} -e "SHUTDOWN;"
 
 exec "$@"
